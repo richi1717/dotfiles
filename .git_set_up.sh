1d068b44afe1844f964c06f2fd80d2c935fc48d4
Richi Jeffery <rici1717@gmail.com>
2017-06-28 23:45:46 -0700

    add git set up script because I'm tired of doing it everytime

---
+++ .git_set_up.sh
@@ -0,0 +1,9 @@
+#aliases
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.co checkout
+
+#user
+git config --global user.name "Richi Jeffery"
+git config --global user.email rici1717@gmail.com
