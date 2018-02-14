0735b4527563115afb546201e7cbacaf6a04e0df
Richi Jeffery <rici1717@gmail.com>
2018-02-14 16:54:35 -0700

    add cherry-pick to git aliases

--- .git_set_up.sh
+++ .git_set_up.sh
@@ -3,6 +3,7 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.co checkout
+git config --global alias.cp cherry-pick
 
 #user
 git config --global user.name "Richi Jeffery"
